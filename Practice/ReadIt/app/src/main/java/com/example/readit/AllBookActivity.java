089eb98d88ddc96c80fef1945ecb8fc4ca4af95e
Abhi <abhishekvelekar792000@gmail.com>
2022-04-11 21:38:50 +0530

    Book layout of cardview made

--- Practice/ReadIt/app/src/main/java/com/example/readit/AllBookActivity.java
+++ Practice/ReadIt/app/src/main/java/com/example/readit/AllBookActivity.java
@@ -1,14 +1,18 @@
 package com.example.readit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class AllBookActivity extends AppCompatActivity {
+    private RecyclerView allBookRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_book);
+
+        allBookRV=findViewById(R.id.allBookRecyclerView);
     }
 }
---
+++ Practice/ReadIt/app/src/main/java/com/example/readit/BookAdapter.java
@@ -0,0 +1,5 @@
+package com.example.readit;
+
+public class BookAdapter {
+    
+}
