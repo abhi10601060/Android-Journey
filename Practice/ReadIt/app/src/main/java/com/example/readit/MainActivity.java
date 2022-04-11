db0233d02f4b34362e8e89572eaf65bac7eec787
Abhi <abhishekvelekar792000@gmail.com>
2022-04-11 21:10:08 +0530

    added intent to all books button

--- Practice/ReadIt/app/src/main/java/com/example/readit/MainActivity.java
+++ Practice/ReadIt/app/src/main/java/com/example/readit/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.readit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button allBk , curBk ,wishlist , favBk , about , completeBk;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewInit();
+
+        allBk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AllBookActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void viewInit(){
+        allBk=findViewById(R.id.btn_allBooks);
+        curBk = findViewById(R.id.btn_curBooks);
+        wishlist = findViewById(R.id.btn_wishlist);
+        favBk = findViewById(R.id.btn_favourite);
+        about = findViewById(R.id.btn_about);
+        completeBk =findViewById(R.id.btn_completed);
     }
 }
