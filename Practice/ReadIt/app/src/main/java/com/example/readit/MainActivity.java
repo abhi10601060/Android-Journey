5121702362de647ac61e39cc58ad3af4e82cd766
Abhi <abhishekvelekar792000@gmail.com>
2022-04-15 08:49:26 +0530

    Webview added and linked to about Button

--- Practice/ReadIt/app/src/main/java/com/example/readit/MainActivity.java
+++ Practice/ReadIt/app/src/main/java/com/example/readit/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.readit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -51,6 +53,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent= new Intent(MainActivity.this,CurReadingBooksActivity.class);
                 startActivity(intent);
+            }
+        });
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Design and Developed by Abhi...\n"+
+                        "Check my website for more fun Applications");
+
+                builder.setPositiveButton("VISIT", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent =new Intent(MainActivity.this,WebviewActivity.class);
+                        intent.putExtra("url","https://github.com/abhi10601060/Android-Journey/tree/master");
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("DISMISS", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                builder.create().show();
+
             }
         });
     }
---
+++ Practice/ReadIt/app/src/main/java/com/example/readit/WebviewActivity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.example.readit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebviewActivity extends AppCompatActivity {
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+
+        webview=findViewById(R.id.webView);
+        webview.loadUrl(url);
+        webview.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webview.canGoBack()){
+            webview.goBack();
+        }else {
+            super.onBackPressed();
+        }
+
+    }
+}
